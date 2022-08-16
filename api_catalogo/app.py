41781431431b3f7e44df6575a079651c39c4fb9e
Liliane <liliane.constantino@inmetrics.com.br>
2022-08-16 19:10:47 -0300

    update hosts

--- api_catalogo/app.py
+++ api_catalogo/app.py
@@ -3,4 +3,4 @@ from flask import Flask
 app = Flask(__name__)
 
 if __name__ == "__main__": 
-    app.run(port=5003, debug=True) 
+    app.run(port=5003, debug=True, host='0.0.0.0') 
--- api_enderecos/app.py
+++ api_enderecos/app.py
@@ -3,4 +3,4 @@ from flask import Flask
 app = Flask(__name__)
 
 if __name__ == "__main__": 
-    app.run(port=5001, debug=True) #indica a porta que a api deve rodar e ativa o modo debug
+    app.run(port=5001, debug=True, host='0.0.0.0') 
